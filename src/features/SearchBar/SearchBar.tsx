c7a956efe224c33b2590a85b51d4bb999ae5145f
Igor Barbashov <bis200705@gmail.com>
2025-07-13 14:33:11 +0300

    feat: make SearchBar feature interactive

--- src/features/SearchBar/SearchBar.tsx
+++ src/features/SearchBar/SearchBar.tsx
@@ -1,8 +1,22 @@
-import { PureComponent } from 'react';
+import { type ChangeEvent, PureComponent } from 'react';
 import type { SearchBarProps } from './searchBar.models';
 
 export class SearchBar extends PureComponent<SearchBarProps> {
+  onSearchStringChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    this.props.onSearchStringChange(e.target.value);
+  };
+
   render() {
-    return <div>SearchBar</div>;
+    const { searchString, onSearchButtonClick } = this.props;
+
+    return (
+      <div>
+        <input
+          value={searchString}
+          onChange={this.onSearchStringChangeHandler}
+        />
+        <button onClick={onSearchButtonClick}>Искать</button>
+      </div>
+    );
   }
 }
