4164d874d6af37cddb5bd3452517a225569818bb
Igor Barbashov <bis200705@gmail.com>
2025-07-13 14:23:44 +0300

    feat: add BookList feature

---
+++ src/features/BookList/BookList.tsx
@@ -0,0 +1,8 @@
+import { PureComponent } from 'react';
+import type { BookListProps } from './bookList.model.ts';
+
+export class BookList extends PureComponent<BookListProps> {
+  render() {
+    return <div>BookList</div>;
+  }
+}
---
+++ src/features/BookList/bookList.model.ts
@@ -0,0 +1,5 @@
+import type { BookDto } from '@entities/Book';
+
+export interface BookListProps {
+  list: BookDto[];
+}
